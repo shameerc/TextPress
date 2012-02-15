53c0d20f7ef65a5e61d7342a512e96d5179958c0
Shameer C <shameer806@gmail.com>
2012-02-15 11:06:29 +0530

    disabled google analytics

--- config/config.php
+++ config/config.php
@@ -11,7 +11,7 @@ return array(
 	'disqus.username' => 'textpress',   // Your disqus username or false (Global)
 	'markdown'		=> true, //Enable of disable markdown parsing. 
 	'base.directory'  => '',
-	'google.analytics' => 'UA-29129685-1', // Google analytics code. set false to disable
+	'google.analytics' => false, // Google analytics code. set false to disable
 	// Define routes
 	'routes' => array(
 			// Site root

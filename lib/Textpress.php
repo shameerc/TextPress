7a112a1a504c6f555730220b1bf0ae23dd2864f6
René Moser <mail@renemoser.net>
2012-04-16 20:57:28 +0200

    Improved slugize to work with umlauts

--- lib/Textpress.php
+++ lib/Textpress.php
@@ -321,13 +321,26 @@ class Textpress
 	* @var String article title
 	* @return String slug
 	*/
-	public function slugize($string)
+	public function slugize($str)
 	{
-		$slug = strtolower(trim($string));
-		$find = array(' ', '&', '\r\n', '\n', '+',',');
-		$slug = str_replace ($find, '-', $slug);
-		return $slug;
-	}
+		$str = strtolower(trim($str));
+ 		
+ 		$chars = array("ä", "ö", "ü", "ß");
+   		$replacements = array("ae", "oe", "ue", "ss");
+		$str = str_replace($chars, $replacements, $str);
+
+		$pattern = array("/(é|è|ë|ê)/", "/(ó|ò|ö|ô)/", "/(ú|ù|ü|û)/");
+   		$replacements = array("e", "o", "u");
+		$str = preg_replace($pattern, $replacements, $str);
+
+		$pattern = array(":", "!", "?", ".", "/", "'");
+		$str = str_replace($pattern, "", $str);
+		
+		$pattern = array("/[^a-z0-9-]/", "/-+/");
+		$str = preg_replace($pattern, "-", $str);
+		
+		return $str;
+    }
 
 	/**
 	* Set config values to View

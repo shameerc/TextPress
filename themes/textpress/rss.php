d17303b200e4af4cda05d0ebd7e530a84a861193
Shameer C <shameer806@gmail.com>
2013-07-28 10:20:47 +0530

    fixing xml validation errors

--- themes/textpress/rss.php
+++ themes/textpress/rss.php
@@ -5,25 +5,25 @@ $lastBuildDate = "";
 if($articles) {
         reset($articles);
         $key = key($articles);
-	$lastBuildDate = date('c', strtotime($articles[$key]['meta']['date']));
+        $lastBuildDate = date(DATE_RSS, strtotime($articles[$key]['meta']['date']));
 }
 ?>
 <rss version="2.0">
 <channel>
-        <title><?php echo $global['site.name']; ?></title>
-        <description><?php echo $global['site.title']; ?></description>
-        <link><?php echo $_SERVER['HTTP_HOST']; ?></link>
+        <title><?php echo htmlspecialchars($global['site.name']); ?></title>
+        <description><?php echo htmlspecialchars($global['site.title']); ?></description>
+        <link>http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST']); ?></link>
         <lastBuildDate><?php echo $lastBuildDate; ?></lastBuildDate>
         <pubDate><?php echo $lastBuildDate; ?></pubDate>
         <ttl>1800</ttl>
 <?php if($articles): ?>
 <?php foreach($articles as $article): ?>
         <item>
-                <title><?php echo $article['meta']['title']; ?></title>
-                <description><?php echo substr(strip_tags($article['content']), 0,300); ?>...</description>
-                <link>http://<?php echo $_SERVER['HTTP_HOST'].$article['url']; ?></link>
-                <guid isPermaLink="true"><?php echo $_SERVER['HTTP_HOST'].$article['url']; ?></guid>
-                <pubDate><?php echo date('c', strtotime($article['meta']['date']));?></pubDate>
+                <title><?php echo htmlspecialchars($article['meta']['title']); ?></title>
+                <description><?php echo htmlspecialchars(substr(strip_tags($article['content']), 0,300)); ?>...</description>
+                <link>http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST'].$article['url']); ?></link>
+                <guid isPermaLink="true">http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST'].$article['url']); ?></guid>
+                <pubDate><?php echo date(DATE_RSS, strtotime($article['meta']['date']));?></pubDate>
         </item>
 <?php endforeach ?>
 <?php endif ?> 

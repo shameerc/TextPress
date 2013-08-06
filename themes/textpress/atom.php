556109d0e300a452cb5b64e16f0914aefe829cd4
Shameer C <shameer806@gmail.com>
2013-08-06 17:31:41 +0530

    fixed feed validation errors

--- themes/textpress/atom.php
+++ themes/textpress/atom.php
@@ -1,32 +1,39 @@
-<?php  
-header("Content-Type:text/xml");  
-echo '<?xml version="1.0" encoding="UTF-8" ?>';
-$lastBuildDate = "";
+<?php
+header("Content-Type:text/xml");
 if($articles) {
-        $article = reset($articles);
-        $lastBuildDate = date('c', strtotime($article['meta']['date']));
+    reset($articles);
+    $key = key($articles);
+    $lastBuildDate = date('c', strtotime($articles[$key]['meta']['date']));
+   // create simplexml object
+    $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><feed xmlns="http://www.w3.org/2005/Atom" />', LIBXML_NOERROR|LIBXML_ERR_NONE|LIBXML_ERR_FATAL);
+    // add channel information
+    
+    $xml->addChild('title', $global['site.name']);
+    
+    $link = $xml->addChild('link');
+    $link->addAttribute("href", "http://" . $_SERVER['HTTP_HOST']);
+    
+    $link = $xml->addChild('link');
+    $link->addAttribute("href", "http://" . $_SERVER['HTTP_HOST'] . "/feed/atom.xml");
+    $link->addAttribute("rel","self");
+
+    $xml->addChild('subtitle', $global['site.title']);
+    $xml->addChild('updated', $lastBuildDate);
+    $xml->addChild('id', "http://" . $_SERVER['HTTP_HOST'] . "/feed/atom");
+    $author = $xml->addChild("author");
+    $author->addChild("name","John Doe");
+    $author->addChild("email","johndoe@example.com");
+    foreach($articles as $article) {
+        $entry = $xml->addChild('entry');
+        $entry->addChild('title', $article['meta']['title']);
+        $link = $entry->addChild('link');
+        $link->addAttribute("href", "http://" . $_SERVER['HTTP_HOST'].$article['url']);
+        $entry->addChild('id', "http://" . $_SERVER['HTTP_HOST'].$article['url']);
+        $entry->addChild("summary");
+        $entry->summary = "<![CDATA[" . substr(strip_tags($article['content']), 0,300) . "]]>";
+        $entry->summary->addAttribute("type","html");
+        $entry->addChild('updated', date('c', strtotime($article['meta']['date'])));
+    }
+    // output xml
+    echo $xml->asXML();
 }
-?>
-<feed xmlns="http://www.w3.org/2005/Atom">
-        <title><?php echo $global['site.name']; ?></title>
-        <subtitle><?php echo htmlspecialchars($global['site.title']); ?></subtitle>
-        <link href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/feed/atom.xml" rel="self" />
-        <link href="http://<?php echo $_SERVER['HTTP_HOST']; ?>" />
-        <id>http://<?php echo $_SERVER['HTTP_HOST']; ?>/feed/atom</id>
-        <updated><?php echo $lastBuildDate; ?></updated>
-        <author>
-                <name>Shameer C</name>
-                <email>shameer@example.com</email>
-        </author>
-<?php if($articles): ?>
-<?php foreach($articles as $article): ?>
-        <entry>
-                <title><?php echo htmlspecialchars($article['meta']['title']); ?></title>
-                <link href="http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST'].$article['url']); ?>" />
-                <id>http://<?php echo $_SERVER['HTTP_HOST'].$article['url']; ?></id>
-                <updated><?php echo date('c', strtotime($article['meta']['date']));?></updated>
-                <summary><?php echo htmlspecialchars(substr(strip_tags($article['content']), 0,300)); ?>...</summary>
-        </entry>
-<?php endforeach ?>
-<?php endif ?> 
-</feed>
--- themes/textpress/rss.php
+++ themes/textpress/rss.php
@@ -1,31 +1,26 @@
 <?php  
 header("Content-Type:text/xml");
-echo '<?xml version="1.0" encoding="UTF-8" ?>';
-$lastBuildDate = "";
 if($articles) {
-        reset($articles);
-        $key = key($articles);
-        $lastBuildDate = date(DATE_RSS, strtotime($articles[$key]['meta']['date']));
+    reset($articles);
+    $key = key($articles);
+    $lastBuildDate = date(DATE_RSS, strtotime($articles[$key]['meta']['date']));
+   // create simplexml object 
+    $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" />', LIBXML_NOERROR|LIBXML_ERR_NONE|LIBXML_ERR_FATAL);
+    // add channel information 
+    $xml->addChild('channel'); 
+    $xml->channel->addChild('title', $global['site.name']); 
+    $xml->channel->addChild('link', "http://" . $_SERVER['HTTP_HOST']); 
+    $xml->channel->addChild('description', $global['site.title']); 
+    $xml->channel->addChild('pubDate', $lastBuildDate); 
+    foreach($articles as $article) { 
+        $item = $xml->channel->addChild('item'); 
+        $item->addChild('title', $article['meta']['title']); 
+        $item->addChild('link', "http://" . $_SERVER['HTTP_HOST'].$article['url']); 
+        $guid = $item->addChild('guid', "http://" . $_SERVER['HTTP_HOST'].$article['url']); 
+        $guid->addAttribute("isPermaLink",'false');
+        $item->description = "<![CDATA[" . substr(strip_tags($article['content']), 0,300) . "]]>"; 
+        $item->addChild('pubDate', date(DATE_RSS, strtotime($article['meta']['date']))); 
+    } 
+    // output xml
+    echo $xml->asXML(); 
 }
-?>
-<rss version="2.0">
-<channel>
-        <title><?php echo htmlspecialchars($global['site.name']); ?></title>
-        <description><?php echo htmlspecialchars($global['site.title']); ?></description>
-        <link>http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST']); ?></link>
-        <lastBuildDate><?php echo $lastBuildDate; ?></lastBuildDate>
-        <pubDate><?php echo $lastBuildDate; ?></pubDate>
-        <ttl>1800</ttl>
-<?php if($articles): ?>
-<?php foreach($articles as $article): ?>
-        <item>
-                <title><?php echo htmlspecialchars($article['meta']['title']); ?></title>
-                <description><?php echo htmlspecialchars(substr(strip_tags($article['content']), 0,300)); ?>...</description>
-                <link>http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST'].$article['url']); ?></link>
-                <guid isPermaLink="true">http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST'].$article['url']); ?></guid>
-                <pubDate><?php echo date(DATE_RSS, strtotime($article['meta']['date']));?></pubDate>
-        </item>
-<?php endforeach ?>
-<?php endif ?> 
-</channel>
-</rss>

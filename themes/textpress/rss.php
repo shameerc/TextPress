9c6fffcf07062238874d5c918deb59fb3f8a8b05
Shameer C <shameer806@gmail.com>
2014-02-22 11:24:07 +0400

    fixed errors in feed

--- themes/textpress/rss.php
+++ themes/textpress/rss.php
@@ -3,7 +3,7 @@ header("Content-Type:text/xml");
 if($articles) {
     reset($articles);
     $key = key($articles);
-    $lastBuildDate = date(DATE_RSS, strtotime($articles[$key]['meta']['date']));
+    $lastBuildDate = date(DATE_RSS, strtotime($articles[$key]->getDate()));
    // create simplexml object 
     $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" />', LIBXML_NOERROR|LIBXML_ERR_NONE|LIBXML_ERR_FATAL);
     // add channel information 
@@ -14,12 +14,12 @@ if($articles) {
     $xml->channel->addChild('pubDate', $lastBuildDate); 
     foreach($articles as $article) { 
         $item = $xml->channel->addChild('item'); 
-        $item->addChild('title', $article['meta']['title']); 
-        $item->addChild('link', "http://" . $_SERVER['HTTP_HOST'].$article['url']); 
-        $guid = $item->addChild('guid', "http://" . $_SERVER['HTTP_HOST'].$article['url']); 
+        $item->addChild('title', $article->getTitle()); 
+        $item->addChild('link', "http://" . $_SERVER['HTTP_HOST'].$article->getUrl()); 
+        $guid = $item->addChild('guid', "http://" . $_SERVER['HTTP_HOST'].$article->getUrl()); 
         $guid->addAttribute("isPermaLink",'false');
-        $item->description = "<![CDATA[" . substr(strip_tags($article['content']), 0,300) . "]]>"; 
-        $item->addChild('pubDate', date(DATE_RSS, strtotime($article['meta']['date']))); 
+        $item->description = "<![CDATA[" . substr(strip_tags($article->getContent()), 0,300) . "]]>"; 
+        $item->addChild('pubDate', date(DATE_RSS, strtotime($article->getDate()))); 
     } 
     // output xml
     echo $xml->asXML(); 

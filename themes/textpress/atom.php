998446f729953f8cf35d12c927c25d816a958892
Shameer C <shameer806@gmail.com>
2014-03-08 13:51:26 +0400

    fixed issue in feeds

--- themes/textpress/atom.php
+++ themes/textpress/atom.php
@@ -3,7 +3,7 @@ header("Content-Type:text/xml");
 if($articles) {
     reset($articles);
     $key = key($articles);
-    $lastBuildDate = date('c', strtotime($articles[$key]['meta']['date']));
+    $lastBuildDate = date('c', strtotime($articles[$key]->getDate()));
    // create simplexml object
     $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><feed xmlns="http://www.w3.org/2005/Atom" />', LIBXML_NOERROR|LIBXML_ERR_NONE|LIBXML_ERR_FATAL);
     // add channel information
@@ -25,14 +25,14 @@ if($articles) {
     $author->addChild("email","johndoe@example.com");
     foreach($articles as $article) {
         $entry = $xml->addChild('entry');
-        $entry->addChild('title', $article['meta']['title']);
+        $entry->addChild('title', $article->getTitle());
         $link = $entry->addChild('link');
-        $link->addAttribute("href", "http://" . $_SERVER['HTTP_HOST'].$article['url']);
-        $entry->addChild('id', "http://" . $_SERVER['HTTP_HOST'].$article['url']);
+        $link->addAttribute("href", "http://" . $_SERVER['HTTP_HOST'].$article->getUrl());
+        $entry->addChild('id', "http://" . $_SERVER['HTTP_HOST'].$article->getUrl());
         $entry->addChild("summary");
-        $entry->summary = "<![CDATA[" . substr(strip_tags($article['content']), 0,300) . "]]>";
+        $entry->summary = "<![CDATA[" . substr(strip_tags($article->getContent()), 0,300) . "]]>";
         $entry->summary->addAttribute("type","html");
-        $entry->addChild('updated', date('c', strtotime($article['meta']['date'])));
+        $entry->addChild('updated', date('c', strtotime($article->getDate())));
     }
     // output xml
     echo $xml->asXML();
